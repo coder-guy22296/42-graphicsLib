55843d50d7fe529c6ba17d85e35b8dfefed0c9c5
Can Yildirim <cyildiri@student.42.us.org>
2017-01-05 17:28:14 -0800

    refactor the gradient line code for clarity

--- drawline.c
+++ drawline.c
@@ -14,151 +14,63 @@
 #include <stdlib.h>
 #include "libgraphics.h"
 #include <stdio.h> // REMOVE
+
+#define ALPHA 24
 #define RED 16
 #define GREEN 8
 #define BLUE 0
 
 
-
-
-int	blend(int color_a, int color_b, float percentage)
+typedef struct	s_color
 {
-	float red_a = (color_a & (255 << 16)) >> 16;
-	float green_a = (color_a & (255 << 8)) >> 8;
-	float blue_a = (color_a & (255 << 0));
+	int			red;
+	int			green;
+	int			blue;
+	int			alpha;
+}				t_color;
 
-	float red_b = (color_b & (255 << 16)) >> 16;
-	float green_b = (color_b & (255 << 8)) >> 8;
-	float blue_b = (color_b & (255 << 0));
+int	extract_byte(int target, int bits_from_right)
+{
+	return ((target & (255 << bits_from_right)) >> bits_from_right);
+}
 
-	float dr;
-	float dg;
-	float db;
-	int blend = 0;
+t_color	color(int color32)
+{
+	t_color str_color;
+	
+	str_color.red = extract_byte(color32, RED);
+	str_color.green = extract_byte(color32, GREEN);
+	str_color.blue = extract_byte(color32, BLUE);
+	str_color.alpha = extract_byte(color32, ALPHA);
+	return (str_color);
+}
 
-	// printf("percentage: %f\n", percentage);
+int		blend(int color_a, int color_b, float percentage)
+{
+	t_color	str_color1;
+	t_color	str_color2;
+	t_color	delta;
+	int 	blend;
+	
+	blend = 0;
+	str_color1 = color(color_a);
+	str_color2 = color(color_b);
 	if (color_a == color_b || percentage < 0.000001)
 		return (color_a);
-	dr = fabsf((red_b - red_a));
-	dg = fabsf((green_b - green_a));
-	db = fabsf((blue_b - blue_a));
+	delta.red = abs((str_color2.red - str_color1.red));
+	delta.green = abs((str_color2.green - str_color1.green));
+	delta.blue = abs((str_color2.blue - str_color1.blue));
 	if (color_a < color_b)
-		blend = (((int)(red_a + (dr * percentage)))<<16) | (((int)(green_a + (dg * percentage)))<<8) | ((int)(blue_a + (db * percentage)));
+		blend = (((int)(str_color1.red + (delta.red * percentage)))<<16)
+				| (((int)(str_color1.green + (delta.green * percentage)))<<8)
+				| ((int)(str_color1.blue + (delta.blue * percentage)));
 	else if (color_a > color_b)
-		blend = (((int)(red_a - (dr * percentage)))<<16) | (((int)(green_a - (dg * percentage)))<<8) | ((int)(blue_a - (db * percentage)));
+		blend = (((int)(str_color1.red - (delta.red * percentage)))<<16)
+				| (((int)(str_color1.green - (delta.green * percentage)))<<8)
+				| ((int)(str_color1.blue - (delta.blue * percentage)));
 	return (blend);
 }
 
-//int	blend(int color_a, int color_b, int steps, int start,  int cur) {
-//	static int delta;
-//
-//	float red_a = (color_a & (255 << 16)) >> 16;
-//	float green_a = (color_a & (255 << 8)) >> 8;
-//	float blue_a = (color_a & (255 << 0));
-//
-//	float red_b = (color_b & (255 << 16)) >> 16;
-//	float green_b = (color_b & (255 << 8)) >> 8;
-//	float blue_b = (color_b & (255 << 0));
-//
-//	float dr;
-//	float dg;
-//	float db;
-//
-//
-//
-//	int blend;
-//
-//	cur = abs(cur - start);
-//
-//
-//	dr = fabsf((red_b - red_a) / steps);
-//	dg = fabsf((green_b - green_a) / steps);
-//	db = fabsf((blue_b - blue_a) / steps);
-//
-//	//blend = color_a | ((red_a + (dr*cur))<<16) | ((green_a + (dg*cur))<<8) | (blue_a + (db*cur));
-//	 if (color_a < color_b)
-//	 {
-//		blend = (((int)(red_a + (dr*cur)))<<16) | (((int)(green_a + (dg*cur)))<<8) | ((int)(blue_a + (db*cur)));
-//
-//	 }
-//	 else if (color_a > color_b)
-//	 {
-//	 	//dr = (red_a - red_b) / steps;
-//	 	//dg = (green_a - green_b) / steps;
-//	 	//db = (blue_a - blue_b) / steps;
-//	 	//blend = color_b | ((red_b - (dr*cur))<<16) | ((green_b - (dg*cur))<<8) | (blue_b - (db*cur));
-//		blend = (((int)(red_a - (dr*cur)))<<16) | (((int)(green_a - (dg*cur)))<<8) | ((int)(blue_a - (db*cur)));
-//
-//	 }
-//	 else
-//	 	return (color_a);
-//
-//	if (blend == 0x00000000)
-//		ft_putstr("RENDERED BLACK PIXEL!!!!\n");
-//
-//	if (color_a == 0x4F000000 || color_b == 0x4F000000)
-//	{
-//		ft_putstr("\t\t\t\tbad line attempt!!!!!!!!!!!!!!\n");
-//	}
-//
-////	ft_putstr("cur: ");
-////	ft_putnbr(cur);
-////
-////	ft_putstr("\t\tsteps: ");
-////	ft_putnbr(steps);
-////
-////	ft_putstr("\t\tdelta red: ");
-////	ft_putnbr(dr);
-////
-////	ft_putstr("\t\tdelta green: ");
-////	ft_putnbr(dg);
-////
-////	ft_putstr("\t\tdelta blue: ");
-////	ft_putnbr(db);
-////
-////	ft_putstr("\t\tred: ");
-////	ft_putnbr((blend & (255 << 16)) >> 16);
-////
-////	ft_putstr("\t\tgreen: ");
-////	ft_putnbr((blend & (255 << 8)) >> 8);
-////
-////	ft_putstr("\t\tblue: ");
-////	ft_putnbr((blend & (255 << 0)) >> 0);
-////
-////	ft_putstr("\n");
-////	ft_putstr("color: ");
-////	ft_putnbr(blend);
-////	ft_putstr("\n");
-//
-//	return (blend);
-//	if (color_a > color_b) {
-//		delta = (color_a - color_b) / steps;
-//		return (color_b + (delta * cur));
-//	} else {
-//		delta = (color_b - color_a) / steps;
-//		return (color_a + (delta * cur));
-//	}
-//}
-/*
-int	blend2(t_vec3fc point_a, t_vec3fc point_b, int x, int y) {
-	static int delta;
-
-	int red_a = color_a & (255 << 16) >> 16;
-	int green_a = color_a & (255 << 8) >> 8;
-	int blue_a = color_a & (255 << 0);
-
-	int red_b = color_b & (255 << 16) >> 16;
-	int green_b = color_b & (255 << 8) >> 8;
-	int blue_b = color_b & (255 << 0);
-
-	int dr = (red_b - red_a) / steps;
-	int dg = (green_b - green_a) / steps;
-	int db = (blue_b - blue_a) / steps;
-	int blend = color_a | ((red_a + (dr*cur))<<16) | ((green_a + (dg*cur))<<8) | (blue_a + (db*cur));
-
-	return (blend);
-}
-*/
 void	drawline(t_renderer *renderer, t_vec3fc point_a, t_vec3fc point_b)
 {
 	int		deltax;
